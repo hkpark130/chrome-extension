a8c8a54e74e2d1c5c0c95176595fe4fcced30b23
hyeonkyeong.park <hkpark@direa.co.kr>
2025-04-16 13:48:58 +0900

    url 변경

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://192.168.2.44:8000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://chrome-ext.direa.synology.me";
 
 let accessToken = null;
 
